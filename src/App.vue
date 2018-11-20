61e3d397bb2cc3f322a41e83b3eefcbe5f8c97b7
wjl <yowjl@datamip.com>
2018-11-20 16:01:45 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <li><router-link to="/Home">Home</router-link></li>
+      <li><router-link to="/Cart">Cart</router-link></li>
+    </div>
+    <div class="app-con">
+        <router-view></router-view>
+    </div>
+
+  </div>
+</template>
+<style lang="scss" scoped>
+  @import './assets/common.scss';
+</style>
+<script>
+import './assets/rem.js'
+export default {
+  name: 'App'
+}
+</script>
+
+
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="Home">
+        <li  v-on:click='setStoreId()' v-for="(good, index) in list" :key="index" className='pic'>
+        <img src='../assets/logo.png' alt=""/>
+        <p>{{good.id}}</p>
+        </li>
+        <div className="clearfix"></div>
+    </div>
+</template>
+
+<script>
+// import '../assets/common.scss'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      list:[{id:9,name:1,pic:'../assets/logo.png',state:false},{id:8,name:1,pic:'../assets/logo.png',state:false},
+            {id:7,name:1,pic:'../assets/logo.png',state:false},{id:6,name:1,pic:'../assets/logo.png',state:false},
+            {id:5,name:1,pic:'../assets/logo.png',state:false},{id:4,name:1,pic:'../assets/logo.png',state:false},
+            {id:3,name:3,pic:'../assets/logo.png',state:false},{id:2,name:1,pic:'../assets/logo.png',state:false}]
+    }
+  },
+  mounted(){
+    this.setStore();
+  },
+  computed:{
+
+  },
+  methods:{
+    setStore(){
+      this.$store.state.list = this.list;
+      var str="<li class='swiper-slide' @click='abc(event)' data-index='0'>"+"</li>";
+      // console.log(this.$store.state.list)
+    },
+    setStoreId(){
+      this.$store.state.list[1].id=1;
+    }
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import '../assets/common.scss';
+  @import '../assets/Home.scss'
+</style>
+
+
+
